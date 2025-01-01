384a803617d4a58d1bdb6c961970857f8123afa3
Akhil351 <akhil.vathaluru@gmail.com>
2025-01-01 22:38:50 +0530

    checking the commit

--- E-commerce(Front-End)/project/src/main.jsx
+++ E-commerce(Front-End)/project/src/main.jsx
@@ -3,9 +3,9 @@ import App from "./App.jsx";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
 createRoot(document.getElementById("root")).render(
+  
   <>
-  <Toaster position="top-center" />
-  <App/>
+    <Toaster position="top-center" />
+    <App />
   </>
-)
-;
+);
